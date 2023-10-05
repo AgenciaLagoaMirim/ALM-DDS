80bea3d4a06f3aee2d2197cf64f8d99da09ba2f0
Jamilson do Nascimento <jamil.pyhidrodev@gmail.com>
2023-10-05 16:06:35 -0300

    config: atualiza DATABASES em settings.py

--- alm_dds/settings.py
+++ alm_dds/settings.py
@@ -103,7 +103,7 @@ WSGI_APPLICATION = "alm_dds.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": "db.sqlite3",
     }
 }
 
